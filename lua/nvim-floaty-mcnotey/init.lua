3b169baea52ed5282153e4cc46e6c4e84ac1316f
wxllxm <william.deeley@hotmail.com>
2024-06-01 15:49:21 +0100

    quick update to centering of floaty window

--- lua/nvim-floaty-mcnotey/init.lua
+++ lua/nvim-floaty-mcnotey/init.lua
@@ -46,57 +46,57 @@ local function get_buffer()
 end
 
 local function create_floaty_window()
-  local height = vim.o.lines
-  local width = vim.o.columns
+	local height = vim.o.lines
+	local width = vim.o.columns
 
-  -- 80% of the editor's height, capped at 30 lines
-  local win_height = math.min(math.ceil(height * 0.7), 50)
-  -- 80% of the editor's width, capped at 150 columns
-  local win_width = math.min(math.ceil(width * 0.8), 190)
+	-- 80% of the editor's height, capped at 30 lines
+	local win_height = math.min(math.ceil(height * 0.7), 50)
+	-- 80% of the editor's width, capped at 150 columns
+	local win_width = math.min(math.ceil(width * 0.8), 190)
 
-  local opts = {
-    style = "minimal",
-    relative = "editor",
-    width = win_width,
-    height = win_height,
-    row = math.ceil((height - win_height) / 2),
-    col = math.ceil((width - win_width) / 2),
-    anchor = "NW",
-    border = {
-      "╭",
-      "─",
-      "╮",
-      "│",
-      "╯",
-      "─",
-      "╰",
-      "│",
-    },
-  }
+	local opts = {
+		style = "minimal",
+		relative = "editor",
+		width = win_width,
+		height = win_height,
+		row = math.ceil((height - win_height) / 2),
+		col = math.ceil((width - win_width) / 2),
+		anchor = "NW",
+		border = {
+			"╭",
+			"─",
+			"╮",
+			"│",
+			"╯",
+			"─",
+			"╰",
+			"│",
+		},
+	}
 
-		local buf = get_buffer()
-		assert(vim.api.nvim_buf_is_valid(buf), "Error: Not a valid buffer.")
+	local buf = get_buffer()
+	assert(vim.api.nvim_buf_is_valid(buf), "Error: Not a valid buffer.")
 
-		-- autocommand to save changes to the original file
-		vim.api.nvim_create_autocmd("BufWritePost", {
-			buffer = buf,
-			callback = function()
-				local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-				local file = io.open(config.notes_path, "w")
-				if file then
-					for _, line in ipairs(lines) do
-						file:write(line, "\n")
-					end
-					file:close()
-				else
-					print("Error: Could not open file for writing")
+	-- autocommand to save changes to the original file
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		buffer = buf,
+		callback = function()
+			local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
+			local file = io.open(config.notes_path, "w")
+			if file then
+				for _, line in ipairs(lines) do
+					file:write(line, "\n")
 				end
-			end,
-		})
+				file:close()
+			else
+				print("Error: Could not open file for writing")
+			end
+		end,
+	})
 
-		return vim.api.nvim_open_win(buf, true, opts)
-	end
+	return vim.api.nvim_open_win(buf, true, opts)
 end
+
 function M:toggle_floaty_mcnotes()
 	local buf = vim.api.nvim_get_current_buf()
 	local buf_name = vim.api.nvim_buf_get_name(buf)
